a66471cdb5b33c376fac0f42ebce81766297a8a0
testlpv <150975298+testlpv@users.noreply.github.com>
2023-11-15 18:01:55 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+variable "name" {}
+variable "location" {}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "vnet_address_spacing" {
+  type = list
+}
+
+variable "subnet_address_prefixes" {
+  type = list
+}
+
+module "networking" {
+  source  = "app.terraform.io/testlp/networking/azurerm"
+  version = "~> 1.0"
+
+  name                    = var.name
+  location                = var.location
+  vnet_address_spacing    = var.vnet_address_spacing
+  subnet_address_prefixes = var.subnet_address_prefixes
+}
